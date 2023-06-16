e56529ad39e2528bd7b66320647b28c40c308015
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-16 18:22:56 +0000

    printing with puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- collect the argument that the user provides and performs inout vaidatio
+ * main- collect the argument to execute
  * return: adds value do it does not return null
  */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

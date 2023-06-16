4eef8a7a9695d1066d683a0739527a68f0979431
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-16 18:07:34 +0000

    use puts to print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<studio.h>
 /**
  * main- collect the argument that the user provides and performs inout vaidatio
@@ -9,16 +8,3 @@ int main(void)
 	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
-=======
-#include <stdio.h>
-/*
- * main- function to return an integer
- * Return: the return make the value 0 
- */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
->>>>>>> 0c648f7acadb2d5009d7af35ad11bacc87362a3c

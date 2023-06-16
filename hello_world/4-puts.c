e42cee615350ba7c05dc116f18663fe61c4a3d0e
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-16 01:10:40 +0000

    used put to print instead of print

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,7 @@
 #!/bin/bash
-int main() { return puts ("Programming is like building a multilingual puzzle"),0;}
+int main(void)
+{
+		puts("Programming is like building a multilingual puzzle");
+
+		return (0);
+}

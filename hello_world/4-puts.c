82dc366a2260decf369f1a2755486723ed76e053
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-16 18:27:44 +0000

    printing with puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - collect the argument to execute
+ * main - collect the argument to execute.
+ *
  * return: adds value do it does not return null
  */
 int main(void)

98349270444a76f1d67a5a88d0e4fbf630363248
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-16 17:49:15 +0000

    print with puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+ * main- collect the argument that the user provides and performs inout vaidatio
+ * return: adds value do it does not return null
+ */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+

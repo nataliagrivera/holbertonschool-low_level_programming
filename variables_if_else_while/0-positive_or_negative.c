ed16aa415bd8a2974dad1128e84b7a0a5e29ac04
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-19 17:42:35 +0000

    print random number every time

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,6 +12,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if(n>0)
-		printf("%d is positive",n)
+		printf("%d is positive",n);
 	return (0);
 }

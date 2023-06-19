c58d7f8c3ac9fd7140b1e4a075db2d139adcb635
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-19 18:35:17 +0000

    is greater than, equal to 0 and exclude

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last = n % 10;
+
+	if (n > 5)
+		printf("Last digits of %d is %d and is greater than 5\n", n, last);
+	else if (n == 0)
+		printf("Last digits of %d is %d and is 0\n", n, last);
+	else if (n != 0 && n < 6)
+		printf("The last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+	return (0);
+}

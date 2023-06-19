d5b01ba7c340532f2e390c21385333397c85d9eb
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-19 15:39:02 +0000

    print if positve

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
-/** Main- to execute funtion */
-
-/* Return: print random number */
+/**
+ * Main - to execute funtion
+ * Return: print random number
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf($n);
+	if(n>0)
+		printf("%d is positive",n)
 	return (0);
 }

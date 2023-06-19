5d75239f80e6c9ca60cf9a94c7f05711ad2476b1
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-19 14:09:10 +0000

    random number generator

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+/** Main- to execute funtion */
+
+/* Return: print random number */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf( %d.\n);
+	return (0);
+}

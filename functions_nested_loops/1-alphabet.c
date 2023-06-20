3ff23a0831638853f74799af7ce20042254e7f53
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-20 22:44:52 +0000

    printing the alphabet in lower case

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	for (c = 97 ; c = 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}

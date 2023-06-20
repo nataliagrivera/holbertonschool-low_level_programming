0b133313608a8ae7f35d859bd6752bb8b9816683
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-20 14:39:42 +0000

    rogram that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
+
+	return (0);
+}

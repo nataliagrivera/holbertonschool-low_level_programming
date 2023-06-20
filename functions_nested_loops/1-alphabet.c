82e467d6ab82089c6b5c33d37c084e00d5835f9f
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-20 23:57:32 +0000

    printing the alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,20 @@
+#include "stdio.h"
 #include "main.h"
 /**
  * main - entry point
  *
  * Return: always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	for (c = 97 ; c = 122; c++)
+
+	char c;
+
+	for (c = 'a' ; c  <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 	_putchar('\n');
 
-	return (0);
 }
+

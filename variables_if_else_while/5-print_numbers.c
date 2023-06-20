f1f9353b7b4b2dd0f83772889fd1cfbf161ce146
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-20 14:17:34 +0000

    print single digit numbers starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int single;
+
+	for (single = 0; single <= 9; single++)
+	{
+		putchar(single);
+	}
+	putchar('\n');
+	return (0);
+}

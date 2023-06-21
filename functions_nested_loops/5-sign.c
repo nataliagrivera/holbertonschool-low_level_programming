79eeb8d60e6b2817365f83e8876a1d9bd342e4b5
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-21 14:08:42 +0000

    adding main file and printing sign of number

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - entry point
+ *
+ *@n: hold int value
+ *
+ * Return: 1, -1, 0
+ *
+ */
+int print_sign(int n)
+{
+
+	n = print_sign(n);
+
+	if(n > 0)
+	{
+		putchar(+);
+		return (1);
+	}
+
+	if else(n < 0)
+	{
+		putchar(-);
+		return(-1);
+	}
+	else()
+	{
+		putchar(0);
+		return(0);
+	}
+
+}
+

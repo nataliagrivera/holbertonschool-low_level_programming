03fc62a4fda405d21a667f764279c9655832285f
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-21 02:49:33 +0000

    checks for lower case

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ *_islower - entry point
+ *
+ *@c: holds int value
+ *
+ *Return: always 0.
+ */
+int _islower(int c);
+{
+	c = islower(c);
+
+	if (c != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

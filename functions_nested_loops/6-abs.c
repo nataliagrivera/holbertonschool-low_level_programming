a97d48dfa5b4153baf5980254eeb6c894c383238
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-21 15:15:56 +0000

    absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_abs - entry point
+ *
+ * @a:hold int value
+ *
+ *Return: absolute value of a
+ */
+int _abs(int a)
+{
+	if (a > 0)
+	{
+		return (a);
+	}
+
+	else
+	{
+	return (a * -1);
+	}
+}
+

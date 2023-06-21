baef76788866a4b0b49e69d2cb80294803a72582
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-21 00:38:53 +0000

    printing just 10 times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,21 @@
 #include "main.h"
 #include "stdio.h"
-
+/**
+ *void - entry point
+ *
+ *Return: always 0
+ */
 void print_alphabet_x10(void)
 {
 	char c;
+	int n = 0;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	 while (c != 10)
+	do {
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+
+	} while (n < 10);
 }

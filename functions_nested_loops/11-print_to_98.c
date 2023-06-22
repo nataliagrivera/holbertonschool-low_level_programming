f78dc781ddc053ea4640dde428193f46a17d6a16
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-22 15:12:44 +0000

    printig from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.c>
+#include "main.h"
+/**
+ *
+ *
+ */
+int print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+	
+}

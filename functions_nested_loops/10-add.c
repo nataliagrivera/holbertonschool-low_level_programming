83d21e60cc88d184abaec1a4e5fd311a9293728c
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-22 02:43:13 +0000

    return sum'

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.c"
+/**
+ * add - entry point
+ *
+ * @a: holds int value for sum
+ *
+ * @b: holds int value for sum
+ *
+ * Return: sum of a + b
+ */
+
+int add(int a, int b)
+{
+
+	int c;
+	
+	c = a + b;
+
+		return (c);
+
+}

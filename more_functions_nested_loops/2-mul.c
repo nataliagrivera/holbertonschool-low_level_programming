ecc374ad45fb22ca731b4e52fcf440950c3b08ff
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-23 18:45:53 +0000

    multiplying int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  mul - multiplies integerss
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the 2 inputs
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

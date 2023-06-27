5116d3ecd4bcc7bbdaf2489c6a92b4517a19a694
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-27 18:50:21 +0000

    reversing the string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *rev_string - reversing string
+ *
+ *@s: string to be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	int a = 0, b;
+	char c;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b < a--; b++)
+	{
+		c = s[a];
+		s[a] = s[b];
+		s[b] = c;
+	}
+}

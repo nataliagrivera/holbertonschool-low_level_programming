8609c30a58862e520bd1816964b57a38139ffa7b
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-28 21:12:37 +0000

    just checking before submiting

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,33 +1,29 @@
 #include "main.h"
 /**
- *puts_half = function that print half
- *
- * @str: string to be printed
- */
+ *  * puts_half - function that prints half
+ *   *
+ *    * @str: string to be printed
+ *     */
 void puts_half(char *str)
 {
 	int a = 0;
 	int b;
 
-	while (str[a] != '\0')
-	{
+	while (str[a] != '\0') {
 		a++;
 	}
 
-	if(a % 2 == 1)
-	{	
-		b = (a - 1)/2;
+	if (a % 2 == 1) {
+		b = (a - 1) / 2;
 		b += 1;
+	} else {
+		b = a / 2;
 	}
 
-		else
-		{
-			b = a / 2;
-		}
-
-	for(b = 0; b < a; b++);
-	{
-		_putchar(str[a]);
+	for (b = 0; b < a; b++) {
+		_putchar(str[b]);
 	}
+
 	_putchar('\n');
 }
+

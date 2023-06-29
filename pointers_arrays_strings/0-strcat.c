9a8000c1533d93bf87b7bd891c33e64458ae0676
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-29 22:25:44 +0000

    checking code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,18 @@
  * Return: a pointer to the dest string
  */
 
-#include "main.h"
-
 char *_strcat(char *dest, char *src)
 {
-	int a, b;
+	int a;
+	int b;
 
 	for (a = 0; dest[a] != '\0'; a++)
-	for (b = 0; src[b] != '\0'; b++)
 	{
-		dest[a++] = src[b];
 	}
+		for (b = 0; src[b] != '\0'; b++)
+		{
+			dest[a++] = src[b];
+		}
 	dest[a] = '\0';
 
 	return (dest);

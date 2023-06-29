a44c75a265a718d3114d5d68424f44a8d2afd312
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-29 18:57:40 +0000

    checking code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,28 @@
 #include "main.h"
+
 /**
- *_strcat - contatanates two strings
+ **_strcat - concatenates two strings
  *
  *@dest: string to concatanate
  *
- * @char: string to concatnate
+ * @src: string to concatnate
+ *
+ * Return: a pointer to the dest string
  */
+
+#include "main.h"
+
 char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
-	for (a = 0; dest[a] != '\0'; a++);
+	for (a = 0; dest[a] != '\0'; a++)
 	for (b = 0; src[b] != '\0'; b++)
-   	{
+	{
 		dest[a++] = src[b];
 	}
 	dest[a] = '\0';
 
-	return dest;
+	return (dest);
 }
 

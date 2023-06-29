21aa35ca2187921798e7078ba1cdd19341d08ad1
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-29 17:33:52 +0000

    checking code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,12 +5,12 @@ char *_strcat(char *dest, char *src)
 	int a, b;
 
 	for (a = 0; dest[a] != '\0'; a++)
-
-		for (b = 0; src[b] != '\0'; b++)
+	{}
+	for (b = 0; src[b] != '\0'; b++)
 		{
-			dest[a + b] = src[b];
-		}
-	dest[a + b] = '\0';
+			dest[a++] = src[b];
 
+			b++;
+		}
 	return (dest);
 }

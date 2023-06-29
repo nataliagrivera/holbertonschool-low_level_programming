c9369514015ae33377a1abed63f203ceae6cb247
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-29 23:54:37 +0000

    checkinig code

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,25 @@
 #include "main.h"
-/**
- **_strncat - function that concatenates two strings
- *
- *@dest: the first string to be added
- *@src: string is appended at the end of dest
- *
- * Return: returns concatenated strings
- */
-char *_strncat(char *dest, char *src)
-{
- 
-	int a; 
 
-	for(a = 0; dest[a] !='\0'; a++)
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of bytes to concatenate from src
+ *
+ * Returns: a pointer to the destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[a]= src[a];
+		dest[i] = src[i];
 	}
 
-	dest[a]= '\0';
+	dest[i] = '\0';
 
-		return(dest);
+	return dest;
 }
 

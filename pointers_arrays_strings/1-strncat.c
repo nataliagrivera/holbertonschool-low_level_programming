0e2d6f3981bacd98adda48e39ff10cddf77ac19e
nataliagrivera <nataliagrivera62@gmail.com>
2023-06-30 00:43:39 +0000

    checkinig code

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,10 @@
-#include "main.h"
+ #include "main.h"
 
 /**
- *_strncat - concatenates two strings
+ * _strncat - concatenates two strings
  *
  * @dest: the destination string
  * @src: the source string
- * @n: the number of bytes to concatenate from src
  *
  * Return: a pointer to the destination string
  */
@@ -13,10 +12,11 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j = 0;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-	dest[i] = src[i];
+	dest[i++] = src[j];
 	}
 
 	dest[i] = '\0';

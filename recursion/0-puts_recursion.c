0808e5e3ec94720e1ac5aff2ca679c3a21b4defb
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-03 19:34:34 +0000

    checking code

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,14 +3,19 @@
  *_puts_recursion - function that recalls itself
  *
  *@*s: string to be printed
+ *
+ *Return: no return
  */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		putchar('\n');
-	return(0);
+	return;
+	}
 
 	putchar(*s);
+	s++
 	_puts_recursion(s + 1);
 
 }

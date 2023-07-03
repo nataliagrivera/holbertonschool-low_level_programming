580f5a300c358269f7e98526682bf48a9d32a268
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-03 20:41:52 +0000

    put recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  *_puts_recursion - function that recalls itself
  *
- *@*s: string to be printed
+ *@s: string to be printed
  *
  *Return: no return
  */
@@ -10,12 +11,12 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
-	return;
+		_putchar('\n');
+		return;
 	}
 
-	putchar(*s);
-	s++
-	_puts_recursion(s + 1);
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 
 }

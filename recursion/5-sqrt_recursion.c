ba5a7fa7df434d7e5825a258820c979856dda2ed
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-07 14:38:30 +0000

    Returns square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *check - checks input
+ *@n:squared number
+ *@b:base of equation
+ *Return: square root of number checked
+ */
+int check(int b, int n)
+{
+
+	if (n * n == b)
+	{
+		return (n);
+	}
+	if (b > n)
+	{
+		return (-1);
+	}
+	return (check(n + 1, b));
+}
+/**
+ *_sqrt_recursion - returns square root of n
+ *
+ *@n: will check square root
+ *
+ *Return: the natural quare root
+ */
+int _sqrt_recursion(int n)
+{
+	return (check(1, n));
+}

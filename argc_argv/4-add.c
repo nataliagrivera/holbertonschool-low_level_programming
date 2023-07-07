94730ee1a128a3e7f84ccb773586d226b2728939
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-07 13:42:10 +0000

    A program that adds

---
+++ argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - program that sums
+ *@argc: counts amount of arguments
+ *@argv: hold arguments
+ *Return: 1 for error and 0 if passed
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int c;
+
+	for (c = 1; c < argc; c++)
+	{
+	a += atoi(argv[c]);
+	}
+
+	if (a == 999)
+	{
+	printf("Error\n");
+	return (1);
+	}
+printf("%d\n", a);
+return (0);
+
+}

81e12791fa6f02c44028cd2e37edb6c1cec9bde4
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-11 19:19:22 +0000

    Remember the read me

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,8 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ *create_array - will create array.
+ *@size: holds lenght of array
+ *@c: charcter that holds value of array
  *
- *
+ * Return: NULL if 0 or NULL, if not string
  */
 char *create_array(unsigned int size, char c)
 {
@@ -10,10 +13,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	pC = malloc(size * sizeof(*pC));
-	
-	if(size == 0)
+
+	if (size == 0)
 	{
-	return(NULL);
+	return (NULL);
 	}
 
 	if (pC == NULL)
@@ -24,7 +27,7 @@ char *create_array(unsigned int size, char c)
 
 	i = 0;
 
-	while(i <size)
+	while (i < size)
 	{
 		pC[i] = c;
 		i++;

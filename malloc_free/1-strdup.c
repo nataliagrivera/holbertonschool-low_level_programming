608313cdcb9874dad5e4616feee0c7ffcf8edb5a
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-12 18:35:03 +0000

    Duplicating string

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -10,19 +10,19 @@
 char *_strdup(char *str)
 {
 
-	int len = strlen(str);
+	int len;
 	char *p;
 	int i = 0;
 
 	if (str == NULL)
 		return (NULL);
-
+	len = strlen(str);
 	p = malloc(sizeof(char) * len + 1);
 
 	if (p == NULL)
 		return (NULL);
 
-	while(i < len)
+	while (i < len)
 	{
 		p[i] = str[i];
 		i++;

1af12967fe024de82ae0df4a2fd19b8014e7b6aa
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-17 14:41:33 +0000

    Structure named dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef STRUCT
+#define STRUCT
+/**
+ *struct dog - structure for dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
 typedef struct dog
 {
-  char* name;
-  float age;
-  char *owner;
-}dog;
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif

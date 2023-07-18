2cc88b58a54feac44000e23a157f24624befabae
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-18 15:09:53 +0000

    adding updated header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef STRUCT
-#define STRUCT
+#ifndef DOG_H
+#define DOG_H
 /**
  *struct dog - structure for dog
  *@name: name of dog
@@ -11,12 +11,9 @@ typedef struct dog
 char *name;
 float age;
 char *owner;
-} dog;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

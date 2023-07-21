0c9d10a8577f8540258264cad19a7ee54d6894c0
nataliagrivera <nataliagrivera62@gmail.com>
2023-07-21 22:30:24 +0000

    returns the sum of all its parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,15 +13,15 @@ unsigned int i;
 int sum;
 va_list arg_sum;
 
-if(n == 0)
-	return(0);
+if (n == 0)
+	return (0);
 va_start(arg_sum, n);
 sum = 0;
 
-for(i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 	sum += va_arg(arg_sum, int);
 
 va_end(arg_sum);
-return(sum);
+return (sum);
 
 }

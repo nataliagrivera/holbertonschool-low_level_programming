d13608691b6675273c291834893abc6f1d651d29
nataliagrivera <nataliagrivera62@gmail.com>
2023-08-02 14:54:25 +0000

    Task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *list_len - gives length of linked list
+ *@h: first node
+ *Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	{
+		while (h != NULL)
+			h = h->next;
+
+		count += 1;
+	}
+
+	return (count);
+}

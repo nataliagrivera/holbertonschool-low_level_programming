66ad976ae6cd30c7db7fb01a1d5619a9b341bd86
nataliagrivera <nataliagrivera62@gmail.com>
2023-08-09 17:28:25 +0000

    Fixing comments in first task

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,27 +6,26 @@
 #include <fcntl.h>
 
 /**
- *read_textfile
- *filename
- *letters
- *
- *
+ *read_textfile - reads a text file and prints it to the POSIX standard output
+ *@filename: string to be used
+ *@letters: amount of actual letters it could read and print
+ *Return: number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, bytes;
 	char *buf = malloc(sizeof(char) * letters);
-	
+
 	if (buf == NULL)
-		return(0);
+		return (0);
 
 	if (filename == NULL)
 	return (0);
 
 	fd = open(filename, O_RDONLY);
-	
+
 	if (fd == -1)
-		return(0);
+		return (0);
 
 	bytes = read(fd, buf, letters);
 
@@ -37,5 +36,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buf);
 	close(fd);
 
-	return(bytes);
+	return (bytes);
 }

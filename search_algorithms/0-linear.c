1c5e08c7ba833a792fa51f4ac29fb2fb4b56eb42
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-20 10:13:58 -0400

    adding Betty documentation

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,7 +13,7 @@
  * Return: first index where value is located
  *  If value is not present in array or if array is NULL, return -1
  */
-*/
+
 int linear_search(int *array, size_t size, int value)
 {
     size_t i;

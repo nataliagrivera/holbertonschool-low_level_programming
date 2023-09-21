8db5ec39b0853e03d67306e43d2b9cc3a626a5f4
nataliagrivera <nataliagrivera62@gmail.com>
2023-09-21 11:25:03 -0400

    fixing binary function

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,34 +12,34 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right = size - 1;
-	size_t i;
+    size_t left = 0;
+    size_t right = size - 1;
+    size_t i;
 
-	if (array == NULL)
-		return (-1);
+    if (array == NULL)
+        return (-1);
 
-	while (left <= right)
-	{
-		size_t mid = left + (right - left) / 2;
+    while (left <= right)
+    {
+        size_t mid = left + (right - left) / 2;
 
-		printf("Searching in array: ");
-		for (i = left; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i < right)
-				printf(", ");
-		}
-		printf("\n");
+        printf("Searching in array: ");
+        for (i = left; i <= right; i++)
+        {
+            printf("%d", array[i]);
+            if (i < right)
+                printf(", ");
+        }
+        printf("\n");
 
-		if (array[mid] == value)
-			return (mid);
+        if (array[mid] == value)
+            return (mid);
 
-		if (array[mid] < value)
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
+        if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
 
-	return (-1);
+    return (-1);
 }
